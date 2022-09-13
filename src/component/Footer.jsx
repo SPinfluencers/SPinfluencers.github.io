1d49169ec106fac830a27de90ba4b0bf7988abcc
Sagar Pawar <100870856+SPinfluencers@users.noreply.github.com>
2022-09-13 13:33:06 +0530

    github pages deployment link

---
+++ src/component/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import logo1 from "./pic/f_logo.png"
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div className='conatiner text-center top'>
+          <div className='img'>
+          <h1>
+              SAG<span style={{color:'#ff014f'}}>AR</span>
+            </h1>
+          </div>
+          <p>© 2021. All rights reserved by SagarCoder-Themes.</p>
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
