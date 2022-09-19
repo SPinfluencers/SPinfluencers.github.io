626213f6caa52296bb12608c5f7d39132fb58b53
Sagar Pawar <100870856+SPinfluencers@users.noreply.github.com>
2022-09-19 20:13:33 +0530

    portfolio

--- src/component/Github/Github.jsx
+++ src/component/Github/Github.jsx
@@ -7,7 +7,7 @@ const Github = () => {
       <h4 style={{textAlign:'center'}}>GITHUB</h4>
       <p style={{textAlign:'center', fontSize: '50px', fontWeight: "bold"}}>Github <span style={{color:'#ff014f'}}>Calendar</span></p>
       <br />
-      <div >
+      <div style={{marginLeft:'200px'}}>
       <GitHubCalendar 
       username='SPinfluencers'
       blockSize={20}
